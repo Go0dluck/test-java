a5436d49f0b66343c6a35fdee5204f5923517002
Dmitriy <dmitriy-ivanoff@bk.ru>
2022-11-06 14:33:00 +0300

    finish

--- src/main/java/com/example/testjava/controllers/BookController.java
+++ src/main/java/com/example/testjava/controllers/BookController.java
@@ -31,7 +31,7 @@ public class BookController {
 
     @PostMapping("")
     public ResponseEntity<HttpStatus> addBook(@RequestBody Book book){
-        bookService.save(book); // test comment
+        bookService.save(book);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
